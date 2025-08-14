484a8bbac14a8289cc886da833d9a5c8a7bd216d
Justyna Gajdek <test@email.com>
2025-08-14 09:55:16 +0000

    add TaskItem subclass

--- src/main/java/com/neueda/todo_simple_app/model/Task.java
+++ src/main/java/com/neueda/todo_simple_app/model/Task.java
@@ -1,5 +1,11 @@
 package com.neueda.todo_simple_app.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
 
     private Long id;
@@ -27,5 +33,16 @@ public class Task {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @OneToMany(mappedBy =  "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    public  List<TaskItem> getTaskItems(){
+        return taskItems;
+    }
+
+    public void setTaskItems(List<TaskItem> taskItems) {
+        this.taskItems = taskItems;
     }
 }
---
+++ src/main/java/com/neueda/todo_simple_app/model/TaskItem.java
@@ -0,0 +1,56 @@
+package com.neueda.todo_simple_app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TaskItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String description;
+    private boolean completed = false;
+
+    @ManyToMany
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+
+    public TaskItem(Long id, String description, boolean completed, Task task) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+        this.task = task;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
